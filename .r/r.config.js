b174d7a8a3729f7b5b31ba28a8c05936e516f9d2
van <1321907687@qq.com>
2015-10-13 14:56:19 +0800

    从 app.js 获取打包的paths

--- .r/r.config.js
+++ .r/r.config.js
@@ -1,59 +1,9 @@
 var util = require('util'),
     _ = require('underscore'),
+    fs = require('fs'),
     defaults = require('./_r.config.json');
 
-var replaceJsSubfix = function(moduleName, path, content) {
-        var _ = {
-                isFunction: function(obj) {
-                    return typeof obj == 'function' || false;
-                },
-                isString: function(obj) {
-                    return typeof obj === 'string' || false;
-                },
-                isArray: function(obj) {
-                    return obj instanceof Array;
-                },
-                isObject: function(obj) {
-                    var type = typeof obj;
-                    return type === 'function' || type === 'object' && !!obj;
-                }
-            },
-            SUBFIX_PARTTERN = /\.js$/ig,
-            SRC_PATH_PARTTERN = /\/company[\/\.]*\/src\//g;
-
-        if (!SRC_PATH_PARTTERN.test(path)) return content;
-
-        var define = function(deps, func) {
-            var result = ['define('];
-
-            if (_.isFunction(deps)) {
-                func = deps;
-            } else {
-                if (_.isString(deps)) {
-                    deps = [deps];
-                }
-                if (_.isArray(deps)) {
-                    var deps_clear = [];
-                    for (var i = 0, l = deps.length; i < l; i++) {
-                        deps_clear.push(deps[i].replace(SUBFIX_PARTTERN, ''));
-                    }
-                    result.push(JSON.stringify(deps_clear) + ',');
-                } else if (_.isObject(deps)) {
-                    func = deps;
-                } else {
-                    throw new Error('undefine parttern:' + content);
-                }
-            }
-
-            result.push(func.toString());
-            result.push(')');
-            return result.join('');
-        };
-
-        content = eval(content);
-        return content;
-    },
-    packages = [{
+var packages = [{
         'name': 'app',
         'location': './',
         'main': 'app.pro'
@@ -75,9 +25,9 @@ var replaceJsSubfix = function(moduleName, path, content) {
             'out': '../dist/src/ng.js',
             'exclude': ['jquery']
         }
-    };
+    },
+    sysModules = ['blog', 'profile', 'index'];
 
-var sysModules = ['blog', 'profile', 'index'];
 for (var i = 0, l = sysModules.length; i < l; i++) {
     var name = sysModules[i];
     packages.push({
@@ -98,6 +48,7 @@ for (var i = 0, l = sysModules.length; i < l; i++) {
     }
 }
 
+loadRequireConfig();
 defaults.packages = packages;
 
 var tmp = null,
@@ -108,3 +59,69 @@ for (var i in modules) {
 }
 
 module.exports = result;
+
+function replaceJsSubfix(moduleName, path, content) {
+    var _ = {
+            isFunction: function(obj) {
+                return typeof obj == 'function' || false;
+            },
+            isString: function(obj) {
+                return typeof obj === 'string' || false;
+            },
+            isArray: function(obj) {
+                return obj instanceof Array;
+            },
+            isObject: function(obj) {
+                var type = typeof obj;
+                return type === 'function' || type === 'object' && !!obj;
+            }
+        },
+        SUBFIX_PARTTERN = /\.js$/ig,
+        SRC_PATH_PARTTERN = /\/company[\/\.]*\/src\//g;
+
+    if (!SRC_PATH_PARTTERN.test(path)) return content;
+
+    var define = function(deps, func) {
+        var result = ['define('];
+
+        if (_.isFunction(deps)) {
+            func = deps;
+        } else {
+            if (_.isString(deps)) {
+                deps = [deps];
+            }
+            if (_.isArray(deps)) {
+                var deps_clear = [];
+                for (var i = 0, l = deps.length; i < l; i++) {
+                    deps_clear.push(deps[i].replace(SUBFIX_PARTTERN, ''));
+                }
+                result.push(JSON.stringify(deps_clear) + ',');
+            } else if (_.isObject(deps)) {
+                func = deps;
+            } else {
+                throw new Error('undefine parttern:' + content);
+            }
+        }
+
+        result.push(func.toString());
+        result.push(')');
+        return result.join('');
+    };
+
+    content = eval(content);
+    return content;
+}
+
+function loadRequireConfig() {
+    var root = fs.readFileSync(defaults.configFile, 'UTF-8'),
+        // fake window define
+        window = {
+            requirejs: function() {}
+        };
+    window.requirejs.config = function(config) {
+        defaults.paths = config.paths;
+        defaults.shim = config.shim;
+    };
+    eval(root);
+    console.log(defaults);
+}
--- app/src/app.js
+++ app/src/app.js
@@ -6,12 +6,13 @@
             // core 依赖
             'ng': './ng',
             'angular': '../libs/angular/angular',
-            'ngAutoValidate': '/libs/angular-auto-validate/dist/jcs-auto-validate',
+            'ngAutoValidate': '../libs/angular-auto-validate/dist/jcs-auto-validate',
             'ngResource': '../libs/angular-resource/angular-resource',
             'ngAnimate': '../libs/angular-animate/angular-animate',
             'ngSanitize': '../libs/angular-sanitize/angular-sanitize',
             'uiRouter': '../libs/angular-ui-router/release/angular-ui-router',
-            'ocLazyLoad': '../libs/ocLazyLoad/dist/ocLazyLoad.require'
+            'ocLazyLoad': '../libs/ocLazyLoad/dist/ocLazyLoad.require',
+            'jquery': '../libs/jquery/dist/jquery'
         },
         shim: {
             'angular': {
